3f34a6e8ed73580643c004cd5c959191a2fb16cb
Khaled Attia <khaled.attia@vodafone.com>
2019-12-25 17:56:51 +0200

    Aeye iOS

---
+++ LoginScreen/Ultility/Extension/UIViewExtension.swift
@@ -0,0 +1,9 @@
+
+import UIKit
+
+extension UIView {
+  func corner(radius: CGFloat = 25) {
+    self.layer.cornerRadius = radius
+    self.layer.masksToBounds = true
+  }
+}
---
+++ LoginScreen/Ultility/LoginService/LoginService.swift
@@ -0,0 +1,5 @@
+
+import Foundation
+protocol LoginService {
+  func login(username: String, password: String, success: @escaping (User, String) -> Void, failure: @escaping (Error?) -> Void)
+}
